f36c498af7ee090b7d33e0d167830df21c803492
kishore <skill2automate@gmail.com>
2025-06-19 07:25:14 +0530

    updated

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -19,4 +19,10 @@ test('has title3', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
+test('has title4', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
 
